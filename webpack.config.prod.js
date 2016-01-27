cd55a156e0e3fad3a6339b29ba9e8d9f2cc56a06
Nikita Frolov <nikita@frolov.fi>
2016-01-27 14:48:59 +0200

    Optimized production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,8 +1,9 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  devtool: 'eval',
+  devtool: 'source-map',
   entry: [
     './src/index'
   ],
@@ -22,7 +23,20 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Exerking'
-    })
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(true),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
   ],
   resolve: {
     modulesDirectories: ['node_modules'],
