a1a8c57555cda2f8a16e115294416dfdc4a72ba9
Marko Laine <kheevo@gmail.com>
2016-02-09 16:25:59 +0200

    action and reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import exercisesReducer from './exercisesReducer';
+import authReducer from './authReducer';
 
 export default function (state = {}, action) {
   return {
     exercises: exercisesReducer(state.exercises, action),
+    auth: authReducer(state.auth, action),
   };
 }
