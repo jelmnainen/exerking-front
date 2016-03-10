036caa611ca098732781900ba893529f0977985a
Marko Laine <kheevo@gmail.com>
2016-03-10 17:49:59 +0200

    submission single view user roles

--- src/components/SubmissionsSingleView.jsx
+++ src/components/SubmissionsSingleView.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import TeacherSection from './smart/TeacherSection';
+import StudentSection from './smart/StudentSection';
 
 export default class SubmissionsSingleView extends Component {
 
@@ -24,10 +26,33 @@ export default class SubmissionsSingleView extends Component {
 
     render() {
       const { exercise, submission } = this.props;
+      let feedback;
+
+      if (submission.feedback_asked) {
+        if (submission.feedback) {
+          feedback = (
+            <div>
+              <h2 className="ui tiny header">Feedback</h2>
+              <p>{submission.feedback}</p>
+            </div>
+          );
+        } else {
+          feedback = (
+            <p>No feedback given</p>
+          );
+        }
+      } else {
+        feedback = (
+          <p>No feedback requested.</p>
+        );
+      }
+
       return (
         <div className="row">
           <div className="column">
-            <h1 className="ui large header">{exercise.title}</h1>
+            <h1 className="ui large header">
+              Submission: {exercise.title}
+            </h1>
             <div className="ui grid">
               <div className="six wide column">
                 <div className="ui segments">
@@ -41,22 +66,34 @@ export default class SubmissionsSingleView extends Component {
               </div>
               <div className="ten wide column">
                 <div className="ui segments">
-                  <div className="ui segment">
-                    <form className="ui form" onSubmit={this.onSubmit}>
-                      <div className="field">
-                        <label>Feedback</label>
-                        <textarea ref="textarea" defaultValue={submission.feedback}></textarea>
-                      </div>
-                      <div className="inline field">
-                        <div className="ui checkbox">
-                          <input type="checkbox" ref="done" defaultChecked={submission.done} />
-                          <label>Done</label>
+                    {submission.done ||
+                      <StudentSection>
+                        <div className="ui attached negative message">
+                          <p>Submission is not accepted</p>
                         </div>
-                      </div>
-                      <button className="ui primary button">
-                        Save
-                      </button>
-                    </form>
+                      </StudentSection>
+                    }
+                  <div className="ui segment">
+                    <TeacherSection>
+                      <form className="ui form" onSubmit={this.onSubmit}>
+                        <div className="field">
+                          <label>Feedback</label>
+                          <textarea ref="textarea" defaultValue={submission.feedback}></textarea>
+                        </div>
+                        <div className="inline field">
+                          <div className="ui checkbox">
+                            <input type="checkbox" ref="done" defaultChecked={submission.done} />
+                            <label>Done</label>
+                          </div>
+                        </div>
+                        <button className="ui primary button">
+                          Save
+                        </button>
+                      </form>
+                    </TeacherSection>
+                    <StudentSection>
+                      {feedback}
+                    </StudentSection>
                   </div>
                 </div>
               </div>
