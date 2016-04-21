37966331db1fcdd2789b0b3ef66a6f7a01e75f41
Lari Saukkonen <lari.saukkonen@helsinki.fi>
2016-04-21 14:11:03 +0300

    test

---
+++ test/usersReducer_spec.js
@@ -0,0 +1,63 @@
+import { expect } from 'chai';
+import { Map, fromJS } from 'immutable';
+
+import reducer from '../src/reducers/usersReducer';
+
+import { USERS_REQUEST, USERS_SUCCESS, USERS_FAIL } from
+  '../src/actions/usersActions';
+import { LOGOUT } from '../src/actions/authActions';
+
+describe('usersReducer', () => {
+  const initialState = fromJS({
+    isFetching: false,
+    isError: false,
+    entries: new Map(),
+  });
+  it('returns correct initial state', () => {
+    const state = fromJS(reducer(undefined, {}));
+    expect(state).to.equal(initialState);
+  });
+  it('changes fetching status correctly', () => {
+    const action = {
+      type: USERS_REQUEST,
+    };
+    const expectedState = initialState.merge({
+      isFetching: true,
+    });
+    const nextState = fromJS(reducer(undefined, action));
+
+    expect(nextState).to.equal(expectedState);
+  });
+  it('populates entries on success', () => {
+    const action = {
+      type: USERS_SUCCESS,
+      payload: [
+        { id: 1 },
+      ],
+    };
+    const expectedState = initialState.merge({
+      entries: new Map(action.payload.map(item => [item.id, fromJS(item)])),
+    });
+    const nextState = fromJS(reducer(undefined, action));
+
+    expect(nextState).to.equal(expectedState);
+  });
+  it('changes error status on fail', () => {
+    const action = {
+      type: USERS_FAIL,
+    };
+    const expectedState = initialState.merge({
+      isError: true,
+    });
+    const nextState = fromJS(reducer(undefined, action));
+    expect(nextState).to.equal(expectedState);
+  });
+  it('clears login info on logout', () => {
+    const action = {
+      type: LOGOUT,
+    };
+    const nextState = reducer(undefined, action);
+
+    expect(nextState).to.equal(initialState);
+  });
+});
